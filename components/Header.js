89fe6404dcdc679a3520b6fb933cb69e689a4a3f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-13 22:45:28 +0200

    Add styling including ackground gradient

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Header = props => {
   return (
-    <View>
-      <Text>{props.title}</Text>
-    </View>
+    <LinearGradient 
+      colors={["#00A1C9", "#0BD68F"]}
+      start={[0, 0]} 
+      end={[1, 1]}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>{props.title}</Text>
+      </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 150,
+    paddingVertical: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    fontSize: 36,
+    color: "#fff",
+    textAlign: 'center'
+  }
+});
 
 export default Header;
