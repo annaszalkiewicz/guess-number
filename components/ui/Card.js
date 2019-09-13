462bbaae14fcdd857403f1bcf9ff3344fbf7359d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-13 23:26:21 +0200

    Create Card ui component

---
+++ components/ui/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const Card = props => {
+  return (
+    <View>{props.children}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Card from "../components/ui/Card";
 
 const StartGameScreen = () => {
   return (
     <View>
       <Text>Start new game</Text>
+      <Card>
+        <Text>Select a number</Text>
+      </Card>
     </View>
   );
 };
