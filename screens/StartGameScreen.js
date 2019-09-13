f5739d7f5195b14106fb38b751a756aef5faa77c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-13 23:41:10 +0200

    Add content to card

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import Card from "../components/ui/Card";
 
 const StartGameScreen = () => {
   return (
     <View>
       <Text>Start new game</Text>
-      <Card>
+      <Card style={styles.card}>
         <Text>Select a number</Text>
+        <TextInput />
+        <View>
+          <Button title="Reset" />
+          <Button title="Confirm" />
+        </View>
       </Card>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    width: 300,
+    maxWidth: '80%'
+  }
+});
 
 export default StartGameScreen;
