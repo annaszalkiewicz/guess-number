0199c141fe2fcd06da4aada0c2b814514e6a87a5
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-14 23:39:49 +0200

    Add submitInputHandler() to set selected number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,50 +13,63 @@ import variables from "../constants/variables";
 import Input from "../components/ui/Input";
 
 const StartGameScreen = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
-  const [inputValue, setInputValue] = useState('');
-  
   const changeInputHandler = inputText => {
-    setInputValue(inputText.replace(/[^0-9]/g, ''))
-  }
+    setInputValue(inputText.replace(/[^0-9]/g, ""));
+  };
 
   const resetInputHandler = () => {
+    setInputValue("");
+    setIsConfirmed(false);
+  };
+
+  const submitInputHandler = () => {
+    let chosenNumber = parseInt(inputValue);
+    setIsConfirmed(true);
+    setSelectedNumber(chosenNumber);
     setInputValue('');
-  }
+  };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Start new game</Text>
-        <Card style={styles.card}>
-          <Text style={styles.text}>Select a number</Text>
-          <View style={styles.inputContainer}>
-            <Input
-              keyboardType="number-pad"
-              style={styles.input}
-              maxLength={2}
-              autoCapitalize="none"
-              autoCorrect={false}
-              blurOnSubmit
-              onChangeText={changeInputHandler}
-              value={inputValue}
-            />
-          </View>
-          <View style={styles.buttonsContainer}>
-            <Button
-              title="Reset"
-              color={variables.secondaryColor}
-              style={styles.button}
-              onPress={resetInputHandler}
-            />
-            <Button
-              title="Confirm"
-              color={variables.primaryColor}
-              style={styles.button}
-            />
-          </View>
-        </Card>
-      </View>
+      <React.Fragment>
+        <View style={styles.container}>
+          <Text style={styles.title}>Start new game</Text>
+          <Card style={styles.card}>
+            <Text style={styles.text}>Select a number</Text>
+            <View style={styles.inputContainer}>
+              <Input
+                keyboardType="number-pad"
+                style={styles.input}
+                maxLength={2}
+                autoCapitalize="none"
+                autoCorrect={false}
+                blurOnSubmit
+                onChangeText={changeInputHandler}
+                value={inputValue}
+              />
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="Reset"
+                color={variables.secondaryColor}
+                style={styles.button}
+                onPress={resetInputHandler}
+              />
+              <Button
+                title="Confirm"
+                color={variables.primaryColor}
+                style={styles.button}
+                onPress={submitInputHandler}
+              />
+            </View>
+          </Card>
+        </View>
+        {isConfirmed && <Text>Confirmed number: {selectedNumber}</Text>}
+      </React.Fragment>
     </TouchableWithoutFeedback>
   );
 };
