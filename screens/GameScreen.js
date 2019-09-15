014c207f764514861a67d170652f05aff6ca12e1
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-15 22:01:57 +0200

    Create generateRandomBetween method

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,6 +2,18 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 const GameScreen = props => {
+
+  const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    if (randomNumber === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return randomNumber;
+    }
+  }
+
   return (
     <View>
       <Text>Game Screen</Text>
