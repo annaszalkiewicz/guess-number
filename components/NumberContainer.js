46192ada2d1ef32dd6c781c4551b7a7f6fe1222e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-15 21:17:08 +0200

    Add styling to utton

--- components/NumberContainer.js
+++ components/NumberContainer.js
@@ -1,40 +1,69 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import Card from './ui/Card';
-import variables from '../constants/variables';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import Card from "./ui/Card";
+import variables from "../constants/variables";
 
 const NumberContainer = props => {
   return (
     <Card style={styles.container}>
       <Text>You selected</Text>
       <Text style={styles.number}>{props.selectedNumber}</Text>
+
       <TouchableOpacity>
-        <View>
-          <Text>Start Game</Text>
-          <MaterialCommunityIcons name="play" />
-        </View>
+        <LinearGradient
+          colors={[variables.secondaryColor, variables.primaryColor]}
+          start={[0, 0]}
+          end={[1, 1]}
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Start Game</Text>
+            <MaterialCommunityIcons
+              name="play"
+              color="#fff"
+              size={20}
+              style={styles.icon}
+            />
+          </View>
+        </LinearGradient>
       </TouchableOpacity>
     </Card>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
     maxWidth: 320,
     marginTop: 20,
-    alignItems: 'center'
+    alignItems: "center"
   },
   number: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: "bold",
     marginVertical: 20,
     padding: 10,
     borderWidth: 1,
-    borderColor: 'rgba(0,0,0,0.3)',
+    borderColor: "rgba(0,0,0,0.3)",
     borderRadius: 10,
-    color: variables.primaryColor
+    color: variables.secondaryColor
+  },
+  button: {
+    padding: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5
+  },
+  buttonText: {
+    color: "#fff",
+    textTransform: "uppercase",
+    marginRight: 5,
+    fontWeight: 'bold'
+  },
+  icon: {
+    marginLeft: 5
   }
 });
-export default NumberContainer
+export default NumberContainer;
