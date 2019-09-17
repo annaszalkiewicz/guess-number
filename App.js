5449240e39d0983b495689d05895a5f94fc6a7b5
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-17 22:46:02 +0200

    Install Raleway Regular and Semibold fonts

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'raleway-regular': require('./assets/fonts/Raleway-Regular_0.ttf'),
+    'raleway-semibold': require('./assets/fonts/Raleway-SemiBold_0.ttf')
+  })
+}
+
 const App = props => {
   const [startGame, setStartGame] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setDataLoaded(true)}
+      onError={error => console.log(error)}
+    />
+  }
 
   startGameHandler = () => {
     setStartGame(true);
