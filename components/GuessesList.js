1799fb3124a53080b9005358bbc90b491e36a15a
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-21 16:53:34 +0200

    Fix displaying list and add styling

--- components/GuessesList.js
+++ components/GuessesList.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 const GuessesList = props => {
-
   const { pastGuesses } = props;
   return (
-    <View>
-      <ScrollView>
-       {pastGuesses.map((guess, index) => {
-         return (
-           <View key={index}>
-             <Text>#{index}</Text>
-             <Text>{guess}</Text>
-           </View>
-         )
-       })}
-      </ScrollView>
-    </View>
-  )
-}
+    <ScrollView contentContainerStyle={styles.container}>
+        {pastGuesses.map((guess, index) => {
+          return (
+            <View key={index} style={styles.listItem}>
+              <Text>#{pastGuesses.length - index}</Text>
+              <Text>{guess}</Text>
+            </View>
+          );
+        })}
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flexGrow: 1
+  },
+  listItem: {
+    width: 300,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: 5,
+    padding: 10,
+    backgroundColor: "#f9f9f9"
+  }
 });
 
 export default GuessesList;
